35c59883650a369d8d1b1d9103d00d6eaa9635be
fukashigiNo <zemvikak@gmail.com>
2026-09-09 23:21:53 +0500

    M: Added new component footewr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/widgets/header";
+import { Footer } from "@/widgets/footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
