305a0bbb33bdf384aa2d315fde1012de7eca84b8
fukashigiNo <zemvikak@gmail.com>
2026-09-11 23:35:18 +0500

    M: Changed styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { Hero } from "@/widgets/hero";
+import { Hero } from "@/widgets/home/home-hero";
+import { HomeForum } from "@/widgets/home/home-forum";
 export default function Home() {
   return (
     <main className="p-20 bg-zinc-900 w-screen flex-1">
       <Hero />
+      <HomeForum />
     </main>
   );
 }
